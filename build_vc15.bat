233fdf9b92f46ffd6cea6c8636b10c528606d4b7
Kroon, Bart <bart.kroon@philips.com>
2019-04-03 13:01:53 +0200

    Remove /WX flag

--- build_vc15.bat
+++ build_vc15.bat
@@ -3,7 +3,7 @@ rem Example script for generating a Visual Studio project for TMIV
 
 mkdir build
 cd build
-set CXXFLAGS=/W4 /WX
+set CXXFLAGS=/W4
 cmake -G "Visual Studio 15 2017 Win64" ..
 cd ..
 cmake --open build 

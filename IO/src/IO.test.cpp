56631a523c9d5b2206c08157da8143294ad18a35
julien.fleureau@technicolor.com <julien.fleureau@technicolor.com>
2019-07-15 16:36:56 +0200

    Remove ITU mention + Discard useless semicolon

--- IO/src/IO.test.cpp
+++ IO/src/IO.test.cpp
@@ -3,7 +3,7 @@
  * and contributor rights, including patent rights, and no such rights are
  * granted under this license.
  *
- * Copyright (c) 2010-2019, ITU/ISO/IEC
+ * Copyright (c) 2010-2019, ISO/IEC
  * All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
@@ -14,7 +14,7 @@
  *  * Redistributions in binary form must reproduce the above copyright notice,
  *    this list of conditions and the following disclaimer in the documentation
  *    and/or other materials provided with the distribution.
- *  * Neither the name of the ITU/ISO/IEC nor the names of its contributors may
+ *  * Neither the name of the ISO/IEC nor the names of its contributors may
  *    be used to endorse or promote products derived from this software without
  *    specific prior written permission.
  *
@@ -68,7 +68,7 @@ auto exampleCamera() -> CameraParameters {
       Vec2f{},                     // perspectiveCenter
       Vec2f{1.f, 100.f}            // depthRange
   };
-};
+}
 
 auto exampleMetadata() -> MivMetadata {
   return {vector<Vec2i>{{4000, 3000}}, // atlasSize
@@ -85,7 +85,7 @@ auto minimalConfig() -> Json {
 }
 )"};
   return Json{stream};
-};
+}
 } // namespace
 
 TEST_CASE("save- and loadMivMetadata") {

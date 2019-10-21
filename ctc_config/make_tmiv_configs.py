83b3bacd3470be5837e176a3eec9d86f35d06dd5
Bart Kroon <bart.kroon@philips.com>
2019-10-21 16:10:35 +0200

    OO way of overriding configurations

--- ctc_config/make_tmiv_configs.py
+++ ctc_config/make_tmiv_configs.py
@@ -16,11 +16,11 @@
 #  * Redistributions in binary form must reproduce the above copyright notice,
 #    this list of conditions and the following disclaimer in the documentation
 #    and/or other materials provided with the distribution.
-#  * Neither the name of the ISO/IEC nor the names of its contributors may
+#  * Neither the seqName of the ISO/IEC nor the names of its contributors may
 #    be used to endorse or promote products derived from this software without
 #    specific prior written permission.
 #
-# THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+# THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS 'AS IS'
 # AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
 # IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
 # ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS
@@ -36,215 +36,326 @@ import json
 import os
 import sys
 
+class DecoderConfiguration:
+	def __init__(self, anchorId, seqId, testPoint):		
+		self.anchorId = anchorId
+		self.seqId = seqId
+		self.testPoint = testPoint
+
+	def numberOfFrames(self):
+		if self.anchorId == 'anchor97':
+			return 97
+		return 17
+
+	def intraPeriod(self):
+		return 32
+
+	def sourceDirectory(self):
+		return '.'
+
+	def seqName(self):
+		return {
+			'A': 'ClassroomVideo',
+			'B': 'TechnicolorMuseum',
+			'C': 'TechnicolorHijack',
+			'D': 'TechnicolorPainter',
+			'E': 'IntelFrog',
+			'J': 'OrangeKitchen',
+			'L': 'PoznanFencing'
+		}[self.seqId]
+
+	def sourceCameraParameters(self):
+		return '{}.json'.format(self.seqName())	
+
+	def viewWidth(self):
+		return {
+			'A': 4096,
+			'B': 2048,
+			'C': 4096,
+			'D': 2048,
+			'E': 1920,
+			'J': 1920,
+			'L': 1920
+		}[self.seqId]
+
+	def viewHeight(self):
+		return {
+			'A': 2048,
+			'B': 2048,
+			'C': 4096,
+			'D': 1088,
+			'E': 1080,
+			'J': 1080,
+			'L': 1080
+		}[self.seqId]
+
+	def atlasWidth(self):
+		return self.viewWidth()
+
+	def atlasHeight(self):
+		return {
+			'A': 3072,
+			'B': 2368,
+			'C': 5120,
+			'D': 1088,
+			'E': 1984,
+			'J': 1080,
+			'L': 1080
+		}[self.seqId]
+
+	def atlasTexturePathFmt(self):
+		return 'ATL_S{}_{}_Tt_c%02d_{}x{}_yuv420p10le.yuv'.format(self.seqId, self.testPoint, self.atlasWidth(), self.atlasHeight())
+
+	def atlasDepthPathFmt(self):
+		return 'ATL_S{}_{}_Td_c%02d_{}x{}_yuv420p10le.yuv'.format(self.seqId, self.testPoint, self.atlasWidth(), self.atlasHeight())
+
+	def atlasMetadataPath(self):
+		return 'ATL_S{}_{}_Tm_c00.bit'.format(self.seqId, self.testPoint)
+
+	def outputDirectory(self):
+		return '.'
+
+	def outputTexturePath(self):
+		return 'TM1_S{}_{}_Tt_%s_{}x{}_yuv420p10le.yuv'.format(seqId, self.testPoint, self.viewWidth(), self.viewHeight())
+
+	def outputCameraName(self):
+		return {
+			'A': 'v1',
+			'B': 'v1',
+			'C': 'v1',
+			'D': 'v1',
+			'E': 'v1',
+			'J': 'v01',
+			'L': 'v01'
+		}[self.seqId]
+
+	def synthesizer(self):
+		return {
+			'rayAngleParameter': 10,
+			'depthParameter': 50,
+			'stretchingParameter': 3,
+			'maxStretching': 5
+		}
+
+	def numberOfSourceViews(self):
+		return {
+			'A': 15,
+			'B': 24,
+			'C': 10,
+			'D': 16,
+			'E': 13,
+			'J': 25,
+			'L': 10
+		}[self.seqId]
+
+	def firstSourceView(self):
+		if self.seqId == 'E':
+			return 1
+		return 0
+
+	def viewNameFormat(self):
+		if seqId == 'J' or seqId == 'L':
+			return 'v{:2}'
+		return 'v{}'
+
+	def sourceCameraNames(self):
+		if self.anchorId == 'view17':
+			return {
+				'A': [ 'v0', 'v7', 'v8', 'v9', 'v10', 'v11', 'v12', 'v13', 'v14' ],
+				'B': [ 'v0', 'v1', 'v4', 'v8', 'v11', 'v12', 'v13', 'v17' ],
+				'C': [ 'v1', 'v4', 'v5', 'v8', 'v9' ],
+				'D': [ 'v0', 'v3', 'v5', 'v6', 'v9', 'v10', 'v12', 'v15' ],
+				'E': [ 'v1', 'v3', 'v5', 'v7', 'v9', 'v11', 'v13' ],
+				'J': [ 'v00', 'v02', 'v04', 'v10', 'v12', 'v14', 'v20', 'v22', 'v24' ],
+				'L': [ 'v00', 'v02', 'v04', 'v06', 'v08' ]
+			}[self.seqId]
+		return list(map(lambda x: self.viewNameFormat().format(x + self.firstSourceView()), range(0, self.numberOfSourceViews())))
+
+	def numberOfCodedSourceViews(self):
+		len(self.sourceCameraNames())
+
+	def useMultipassRenderer(self):
+		return self.anchorId == 'view17'
+
+	def rendererMethod(self):
+		if self.useMultipassRenderer():
+			return 'MultipassRenderer'
+		return 'Renderer'
+
+	def renderer(self):
+		config = {
+			'SynthesizerMethod': 'Synthesizer',
+			'Synthesizer': self.synthesizer(),
+			'InpainterMethod': 'Inpainter',
+			'Inpainter': {}
+		}
+		if self.useMultipassRenderer():
+			config.update({
+				'NumberOfPasses': 3,
+				'NumberOfViewsPerPass': [2, 4, self.numberOfCodedSourceViews()]
+			})
+		return config
+
+	def parameters(self):
+		config = {
+			'numberOfFrames': self.numberOfFrames(),
+			'intraPeriod': self.intraPeriod(),
+			'SourceDirectory': self.sourceDirectory(),
+			'SourceCameraParameters': self.sourceCameraParameters(),
+			'AtlasTexturePathFmt': self.atlasTexturePathFmt(),
+			'AtlasDepthPathFmt': self.atlasDepthPathFmt(),
+			'AtlasMetadataPath': self.atlasMetadataPath(),
+			'OutputDirectory': self.outputDirectory(),
+			'OutputTexturePath': self.outputTexturePath(),
+			'OutputCameraName': self.outputCameraName(),
+			'DecoderMethod': 'Decoder',
+			'Decoder': {
+				'AtlasDeconstructorMethod': 'AtlasDeconstructor',
+				'AtlasDeconstructor': {},
+				'RendererMethod': self.rendererMethod(),
+				self.rendererMethod(): self.renderer()
+			}
+		}
+		return config
+
+	def path(self):
+		return '{}/TMIV_{}_{}_{}.json'.format(self.testPoint, self.anchorId, self.seqId, self.testPoint)
+
+class EncoderConfiguration(DecoderConfiguration):
+	def __init__(self, anchorId, seqId):
+		DecoderConfiguration.__init__(self, anchorId, seqId, 'R0')
+
+	def startFrame(self):
+		# TODO(BK): Decide on startFrame for 17-frame anchors
+		return {
+			'A': 23,
+			'B': 100,
+			'C': 0,
+			'D': 10,
+			'E': 135,
+			'J': 0,
+			'L': 30
+		}[self.seqId]		
+
+	def viewOptimizerMethod(self):
+		if anchorId == 'view17':
+			return 'NoViewOptimizer'
+		return 'ViewReducer'
+
+	def sourceTexturePathFmt(self):
+		return '%s_texture_{}x{}_yuv420p10le.yuv'.format(self.viewWidth(), self.viewHeight())
+
+	def sourceDepthBitDepth(self):
+		if self.seqId == 'J':
+			return 10
+		if self.seqId == 'L':
+			return 8
+		return 16
+
+	def sourceDepthVideoFormat(self):
+		return {
+			8: 'yuv420p',
+			10: 'yuv420p10le',
+			16: 'yuv420p16le'
+		}[self.sourceDepthBitDepth()]
+
+	def sourceDepthPathFmt(self):
+		return '%s_depth_{}x{}_{}.yuv'.format(self.viewWidth(), self.viewHeight(), self.sourceDepthVideoFormat())
+
+	def omafV1CompatibleFlag(self):
+		# Just to do something slightly more interesting then False
+		return self.seqId == 'A' or self.seqId == 'C'
+
+	def pruner(self):
+		config = self.synthesizer()
+		config.update({
+			'maxDepthError': 0.1,
+			'erode': 2,
+			'dilate': 5
+		})
+		return config
+
+	def lumaSamplesPerView(self):
+		return 2 * self.atlasWidth() * self.atlasHeight()
+
+	def maxLumaSamplesPerFrame(self):
+		if self.anchorId == 'anchor97' or self.anchorId == 'anchor17':
+			return {
+				'A': 1,
+				'B': 3,
+				'C': 2,
+				'D': 4,
+				'E': 3,
+				'J': 4,
+				'L': 3
+			}[self.seqId] * self.lumaSamplesPerView()
+		return 0
+
+	def parameters(self):
+		# The encoder configuration includes the decoder configuration for testing.
+		# Enabling reconstruction will run the decoder while encoding.
+		config = DecoderConfiguration.parameters(self)
+		config['reconstruct'] = False
+
+		config['startFrame'] = self.startFrame()
+		config['SourceTexturePathFmt'] = self.sourceTexturePathFmt()
+		config['SourceDepthPathFmt'] = self.sourceDepthPathFmt()
+		config['SourceDepthBitDepth'] = self.sourceDepthBitDepth()
+		config['SourceCameraNames'] = self.sourceCameraNames()
+		config['OmafV1CompatibleFlag'] = self.omafV1CompatibleFlag()
+		config['EncoderMethod'] = 'Encoder'
+		config['Encoder'] = {
+				'ViewOptimizerMethod': self.viewOptimizerMethod(),
+				self.viewOptimizerMethod(): {},
+				'AtlasConstructorMethod': 'AtlasConstructor',
+				'AtlasConstructor': {
+					'PrunerMethod': 'Pruner',
+					'Pruner': self.pruner(),
+					'AggregatorMethod': 'Aggregator',
+					'Aggregator': {},
+					'PackerMethod': 'Packer',
+					'Packer': {
+						'Alignment': 8,
+						'MinPatchSize': 16,
+						'Overlap': 1,
+						'PiP': 1
+					},
+					'AtlasResolution': [
+						self.atlasWidth(),
+						self.atlasHeight()
+					],
+					'MaxLumaSamplesPerFrame': self.maxLumaSamplesPerFrame()
+				}
+			}
+		return config
+		
+	def path(self):
+		return 'TMIV_{}_{}.json'.format(self.anchorId, self.seqId)
+
 def saveJson(data, path):
     with open(path, 'w') as stream:
-        json.dump(data, stream, indent=4, separators=(',', ': '))
-        stream.write('\n')
+        json.dump(data, stream, indent=4, sort_keys=True, separators=(',', ': '))
 
-		
-class ConfigurationParameters:
-	def __init__(self, seqId, anchorType):
-		self.sourceDepthBitDepth = 16
-		self.outputCameraName = "v1"
-				
-		if anchorType == 'anchor97':
-			self.numberOfFrames = 97
-		else:
-			self.numberOfFrames = 17
-			
-		if seqId == "A":
-			self.name = "ClassroomVideo"		
-			self.startFrame = 23
-			self.sourceCameraNames = [ "v0", "v1", "v2", "v3", "v4", "v5", "v6", "v7", "v8", "v9", "v10", "v11", "v12", "v13", "v14" ]
-			sourceCameraNamesViewAnchor = [ "v0", "v7", "v8", "v9", "v10", "v11", "v12", "v13", "v14" ]
-			self.numberOfViewsPerPass = [2,4,9]	
-			self.width = 4096
-			self.height = 2048
-			self.maxLumaSamplesPerFrame = 33554432
-			maxLumaSamplesPerFrameViewAnchor = 150994944
-		elif seqId == "B":
-			self.name = "TechnicolorMuseum"		
-			self.startFrame = 100
-			self.sourceCameraNames = [ "v0", "v1", "v2", "v3", "v4", "v5", "v6", "v7", "v8", "v9", "v10", "v11", "v12", "v13", "v14", "v15", "v16", "v17",  "v18", "v19", "v20", "v21", "v22", "v23" ]
-			sourceCameraNamesViewAnchor = [ "v0", "v1", "v4", "v8", "v11", "v12", "v13", "v17" ]
-			self.numberOfViewsPerPass = [2,4,8]	
-			self.width = 2048
-			self.height = 2048
-			self.maxLumaSamplesPerFrame = 33554432
-			maxLumaSamplesPerFrameViewAnchor = 67108864
-		elif seqId == "C":
-			self.name = "TechnicolorHijack"		
-			self.startFrame = 0
-			self.sourceCameraNames = [ "v0", "v1", "v2", "v3", "v4", "v5", "v6", "v7", "v8", "v9" ]
-			sourceCameraNamesViewAnchor = [ "v1", "v4", "v5", "v8", "v9" ]
-			self.numberOfViewsPerPass = [2,4,5]	
-			self.width = 4096
-			self.height = 4096
-			self.maxLumaSamplesPerFrame = 100663296
-			maxLumaSamplesPerFrameViewAnchor = 167772160
-		elif seqId == "D":
-			self.name = "TechnicolorPainter"		
-			self.startFrame = 10
-			self.sourceCameraNames = [ "v0", "v1", "v2", "v3", "v4", "v5", "v6", "v7", "v8", "v9", "v10", "v11", "v12", "v13", "v14", "v15" ]
-			sourceCameraNamesViewAnchor = [ "v0", "v3", "v5", "v6", "v9", "v10", "v12", "v15" ]
-			self.numberOfViewsPerPass = [2,4,8]	
-			self.width = 2048
-			self.height = 1088
-			self.maxLumaSamplesPerFrame = 22282240
-			maxLumaSamplesPerFrameViewAnchor = 35651584
-		elif seqId == "E":
-			self.name = "IntelFrog"		
-			self.startFrame = 135
-			self.sourceCameraNames = [ "v1", "v2", "v3", "v4", "v5", "v6", "v7", "v8", "v9", "v10", "v11", "v12", "v13" ]
-			sourceCameraNamesViewAnchor = [ "v1", "v3", "v5", "v7", "v9", "v11", "v13" ]
-			self.numberOfViewsPerPass = [2,4,7]	
-			self.width = 1920
-			self.height = 1080
-			self.maxLumaSamplesPerFrame = 41472000
-			maxLumaSamplesPerFrameViewAnchor = 29030400
-		elif seqId == "J":
-			self.name = "OrangeKitchen"		
-			self.startFrame = 0
-			self.sourceCameraNames = [ "v00", "v01", "v02", "v03", "v04", "v05", "v06", "v07", "v08", "v09", "v10", "v11", "v12", "v13", "v14", "v15", "v16", "v17",  "v18", "v19", "v20", "v21", "v22", "v23", "v24" ]
-			sourceCameraNamesViewAnchor = [ "v00", "v02", "v04", "v10", "v12", "v14", "v20", "v22", "v24" ]
-			self.numberOfViewsPerPass = [2,4,7]	
-			self.width = 1920
-			self.height = 1080
-			self.maxLumaSamplesPerFrame = 12441600
-			maxLumaSamplesPerFrameViewAnchor = 37324800
-			self.sourceDepthBitDepth = 10
-			self.outputCameraName = "v01"
-		elif seqId == "L":
-			self.name = "PoznanFencing"		
-			self.startFrame = 30
-			self.sourceCameraNames = [ "v00", "v01", "v02", "v03", "v04", "v05", "v06", "v07", "v08", "v09" ]
-			sourceCameraNamesViewAnchor = [ "v00", "v02", "v04", "v06", "v08" ]
-			self.numberOfViewsPerPass = [2,4,7]	
-			self.width = 1920
-			self.height = 1080
-			self.maxLumaSamplesPerFrame = 16588800
-			maxLumaSamplesPerFrameViewAnchor = 20736000
-			self.sourceDepthBitDepth = 8
-			self.outputCameraName = "v01"
-		else:
-			print("Logic error: unknown sequence id")
-			exit(-2)
-		
-		if anchorType == 'view_anchor17': 
-			self.sourceCameraNames = sourceCameraNamesViewAnchor
-			self.maxLumaSamplesPerFrame = maxLumaSamplesPerFrameViewAnchor
-
-
-	
-def makeConfiguration(anchorType, testPoint, seqId, parameters):
-	p = parameters
-	W = p.width
-	H = p.height
-	
-	if p.sourceDepthBitDepth > 8:
-		depthFmt = "{}le".format(p.sourceDepthBitDepth)
-	else:
-		depthFmt = ""
-	
-	synthesizer = { "rayAngleParameter": 20,
-				"depthParameter": 20,
-				"stretchingParameter": 0.6,
-				"maxStretching": 5 }
-	
-	if anchorType == 'view_anchor17':
-		decoder =  {
-			"AtlasDeconstructorMethod": "AtlasDeconstructor",
-			"AtlasDeconstructor": {},
-			"RendererMethod": "MultipassRenderer",
-			"MultipassRenderer": {
-				"NumberOfPasses": 3,
-				"NumberOfViewsPerPass": p.numberOfViewsPerPass,
-				"SynthesizerMethod": "Synthesizer",
-				"Synthesizer": synthesizer,
-				"InpainterMethod": "Inpainter",
-				"Inpainter": {}
-			}
-		}
-		redundancyFactor = 0.02
-		viewOptimizerMethod = "NoViewOptimizer"
-	else:
-		decoder =  {
-			"AtlasDeconstructorMethod": "AtlasDeconstructor",
-			"AtlasDeconstructor": {},
-			"RendererMethod": "Renderer",
-			"Renderer": {
-				"SynthesizerMethod": "Synthesizer",
-				"Synthesizer": synthesizer,
-				"InpainterMethod": "Inpainter",
-				"Inpainter": {}
-			}
-		}
-		redundancyFactor = 0.05
-		viewOptimizerMethod = "ViewReducer"
-	
-	
-	config = {
-		"startFrame": p.startFrame,
-		"numberOfFrames": p.numberOfFrames,
-		"intraPeriod": 32,
-		"SourceTexturePathFmt": "%s_texture_{}x{}_yuv420p10le.yuv".format(W,H),
-		"SourceDepthPathFmt": "%s_depth_{}x{}_yuv420p{}.yuv".format(W,H,depthFmt),
-		"SourceDepthBitDepth": p.sourceDepthBitDepth,
-		"SourceCameraParameters": "{}.json".format(p.name),
-		"SourceCameraNames": p.sourceCameraNames,
-	    "AtlasTexturePathFmt": "ATL_S{}_{}_Tt_c%02d_{}x{}_yuv420p10le.yuv".format(seqId,testPoint,W,H),
-		"AtlasDepthPathFmt": "ATL_S{}_{}_Td_c%02d_{}x{}_yuv420p10le.yuv".format(seqId,testPoint,W,H),
-		"AtlasMetadataPath": "ATL_S{}_{}_Tm_c00.bit".format(seqId,testPoint),
-		"OutputTexturePath": "TM1_S{}_{}_Tt_%s_{}x{}_yuv420p10le.yuv".format(seqId,testPoint,W,H),
-		"OutputCameraName": p.outputCameraName,
-		"OmafV1CompatibleFlag": False,
-		"EncoderMethod": "Encoder",
-		"Encoder": {
-			"ViewOptimizerMethod": viewOptimizerMethod,
-			"{}".format(viewOptimizerMethod): {},
-			"AtlasConstructorMethod": "AtlasConstructor",
-			"AtlasConstructor": {
-				"PrunerMethod": "Pruner",
-				"Pruner": {
-					"RedundancyFactor": redundancyFactor,
-					"ErosionIter": 1,
-					"DilationIter": 5
-				},
-				"AggregatorMethod": "Aggregator",
-				"Aggregator": {},
-				"PackerMethod": "Packer",
-				"Packer": {
-					"Alignment": 8,
-					"MinPatchSize": 16,
-					"Overlap": 1,
-					"PiP": 1
-				},
-				"AtlasResolution": [
-					W,
-					H
-				],
-				"MaxLumaSamplesPerFrame": p.maxLumaSamplesPerFrame
-			}
-		},
-		"DecoderMethod": "Decoder",
-		"Decoder": decoder
-	}
-	return config
-		
-		
 if __name__ == '__main__':
-
 	if sys.version_info[0] < 3:
-		print ("Error: Python version < 3")
+		print ('Error: Python version < 3')
 		exit(-1)
 
-	for testPoint in ["R0", "QP1", "QP2", "QP3", "QP4", "QP5"]:
+	anchors = [ 'anchor97', 'anchor17', 'view17' ]
+	seqIds = ['A', 'B', 'C', 'D', 'E', 'J', 'L']
+	testPoints = ['R0', 'QP1', 'QP2', 'QP3', 'QP4', 'QP5']
+
+	for anchorId in anchors:
+		for seqId in seqIds:
+			config = EncoderConfiguration(anchorId, seqId)
+			print(config.path())
+			saveJson(config.parameters(), config.path())
+
+	for testPoint in testPoints:
 		if not os.path.exists(testPoint):
 			os.makedirs(testPoint)
-		for anchorType in [ 'anchor97', 'anchor17', 'view_anchor17' ]:
-			for seqId in ["A", "B", "C", "D", "E", "J", "L"]:
-				print(anchorType, testPoint, seqId)
-				parameters = ConfigurationParameters(seqId, anchorType)
-				config = makeConfiguration(anchorType, testPoint, seqId, parameters)	
-				saveJson(config, "{}/TMIV_{}_{}.json".format(testPoint, anchorType, seqId) )
-	
+		for anchorId in anchors:
+			for seqId in seqIds:
+				config = DecoderConfiguration(anchorId, seqId, testPoint)
+				print(config.path())
+				saveJson(config.parameters(), config.path())

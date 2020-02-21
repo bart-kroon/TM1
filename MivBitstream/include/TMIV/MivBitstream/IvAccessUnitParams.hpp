df083f367089645dbbe5d89012a1037051b55be4
Bart Kroon <bart.kroon@philips.com>
2020-02-21 11:53:27 +0100

    Print AFPS and ATGH of IVAU

--- MivBitstream/include/TMIV/MivBitstream/IvAccessUnitParams.hpp
+++ MivBitstream/include/TMIV/MivBitstream/IvAccessUnitParams.hpp
@@ -218,7 +218,7 @@ inline auto AtlasAccessUnitParams::operator==(const AtlasAccessUnitParams &other
 
 inline auto operator<<(std::ostream &stream, const IvAccessUnitParams &x) -> std::ostream & {
   for (auto &atlas : x.atlas) {
-    stream << atlas.asps;
+    stream << atlas.asps << atlas.afps << atlas.atgh;
   }
   stream << "Total number of patches: " << x.patchParamsList.size() << '\n';
   return stream;

b8f7538299c292e52df0c14b27688bcf7e7ed0ac
Bart Kroon <bart.kroon@philips.com>
2020-06-29 13:51:48 +0200

    Delete build_vc15.bat

--- build_vc15.bat
+++
@@ -1,9 +0,0 @@
-@echo off
-rem Example script for generating a Visual Studio project for TMIV
-
-mkdir build
-cd build
-set CXXFLAGS=/W4
-cmake -G "Visual Studio 15 2017 Win64" ..
-cd ..
-cmake --open build 

02cb7b485000d8b5925733010ab1ada709edc56f
Christoph Bachhuber <christoph.bachhuber@nokia.com>
2020-09-09 15:50:16 +0200

    Fix path

--- CMake/catch2.cmake
+++ CMake/catch2.cmake
@@ -1,6 +1,6 @@
 cmake_minimum_required(VERSION 3.10 FATAL_ERROR)
 
-option(BUILD_CATCH2 "Build and use Catch2 in TMIV when available" ON)
+option(BUILD_CATCH2 "Build and use Catch2 for unit tests in TMIV" ON)
 
 if(BUILD_CATCH2)
     SET(CATCH2_ROOT thirdparty/catch2)
@@ -12,5 +12,6 @@ if(BUILD_CATCH2)
         INSTALL_DIR ${CATCH2_ROOT}/install
         INSTALL_COMMAND make DESTDIR=<INSTALL_DIR> install
     )
+    SET(Catch2_DIR ${CMAKE_BINARY_DIR}/${CATCH2_ROOT}/install/usr/local/lib/cmake/Catch2)
 
 endif()

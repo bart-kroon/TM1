968632c2d1dd775d2858f96a01d0f4a88a60e64e
Christoph Bachhuber <christoph.bachhuber@nokia.com>
2020-09-18 16:59:22 +0200

    Revert cmake clang-tidy autofix

--- CMake/clang_tidy.cmake
+++ CMake/clang_tidy.cmake
@@ -3,15 +3,10 @@ cmake_minimum_required(VERSION 3.14 FATAL_ERROR)
 option(ENABLE_CLANG_TIDY "Turn on clang_tidy processing if available" ON)
 
 if (ENABLE_CLANG_TIDY)
-    option(APPLY_CLANG_TIDY_FIXES "Let clang-tidy apply its auto fixes" ON)
     find_program(CLANG_TIDY_PATH NAMES "clang-tidy")
 
     if(CLANG_TIDY_PATH)
-        if(APPLY_CLANG_TIDY_FIXES)
-            set(CMAKE_CXX_CLANG_TIDY "${CLANG_TIDY_PATH}" -fix)
-        else()
-            set(CMAKE_CXX_CLANG_TIDY "${CLANG_TIDY_PATH}")
-        endif()
+        set(CMAKE_CXX_CLANG_TIDY "${CLANG_TIDY_PATH}")
     endif()
 endif()
 

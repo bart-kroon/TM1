d384c2d9584fafa94eed5f082f97e8e4afa4b145
Christoph Bachhuber <christoph.bachhuber@nokia.com>
2020-10-23 15:39:20 +0300

    Warn if clang-tidy is not found

--- CMake/clang_tidy.cmake
+++ CMake/clang_tidy.cmake
@@ -7,6 +7,8 @@ if (ENABLE_CLANG_TIDY)
 
     if(CLANG_TIDY_PATH)
         set(CMAKE_CXX_CLANG_TIDY "${CLANG_TIDY_PATH}")
+    else()
+        message(WARNING "Couldn't find clang-tidy")
     endif()
 endif()
 
